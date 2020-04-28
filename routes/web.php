593f600f9febf4a4be8f242ee15dffdbd06ba9f8
Shadi Abdelslam <shadodev@gmail.com>
2020-04-28 07:40:06 +0200

    Finish Frontend

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('welcome');
-});
-
-// Route::group(['prefix' => 'api'], function () {
-
-// });
+})->where(['any' => '.*']);
