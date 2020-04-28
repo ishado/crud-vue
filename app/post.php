4b61cccd32ed4fa9dfead159bbd123e870d2f8f7
Shadi Abdelslam <shadodev@gmail.com>
2020-04-28 06:27:57 +0200

    Finish Backend API

---
+++ app/post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    // protected $table = 'posts';
+    // public $timestamps = true;
+
+    // protected $fillable = ['title','body'];
+    // protected $hidden = ['created_at', 'updated_at'];
+}
